c9a46d1ccb5d336711640afb188bc68e838e83d7
Philg112 <pgrujovski@protonmail.com>
2023-09-12 21:49:08 +1000

    Initial

--- src/Dudu.hs
+++ src/Dudu.hs
@@ -1,15 +1,8 @@
-{- |
-Copyright: (c) 2023 Philg112
-SPDX-License-Identifier: MIT
-Maintainer: Philg112 <pgrujovski@protonmail.com>
-
-See README for more info
--}
-
 module Dudu
-       ( someFunc
-       ) where
-
+    ( someFunc,
+    )
+where
 
 someFunc :: IO ()
 someFunc = putStrLn ("someFunc" :: String)
+
